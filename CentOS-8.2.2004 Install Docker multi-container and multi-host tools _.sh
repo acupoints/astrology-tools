5df33e8707c9a346cc2a8657a72dcbc81925ff7e
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-29 18:23:15 +0800

    Create CentOS-8.2.2004 Install Docker multi-container and multi-host tools _.sh

---
+++ CentOS-8.2.2004 Install Docker multi-container and multi-host tools _.sh
@@ -0,0 +1,11 @@
+## 安装 docker-compose
+mv astrology-tools/docker-compose-1.27.4/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+## 安装 docker-machine
+mv astrology-tools/docker-machine-v0.16.2/docker-machine-Linux-x86_64 /usr/local/bin/docker-machine
+chmod +x /usr/local/bin/docker-machine
+docker-machine --version
+
+
